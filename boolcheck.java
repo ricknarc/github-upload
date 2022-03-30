1bdfb5181f239073faf72c7a83a883771e6c5958
Saurav <sasaurabh12@gmail.com>
2022-03-30 18:16:29 +0530

    Initial commit

---
+++ boolcheck.java
@@ -0,0 +1,13 @@
+public class boolcheck
+{
+    public static void main(String[]args)
+    {
+        int abc=4, bc=8;
+        boolean isev= (abc>bc);
+        boolean add = (abc-bc > abc);
+        System.out.println(isev);
+        System.out.println(add);
+
+    }
+}
+
---
+++ newpro.java
@@ -0,0 +1,23 @@
+public class newpro {
+    public static void main(String[] args){
+        // 
+        int marks = 79;
+    // Write your code here
+    if (marks>= 90) {
+      System.out.println("excellent");
+      
+    } else if ( marks > 80 && marks < 90) {
+       System.out.println("good");
+    } else if ( marks >=70 && marks<=80 ) {
+
+       System.out.println("fair");
+    }  else if(marks >=60 && marks<70 ){
+        System.out.println("meets expectations");
+    } else {
+       System.out.println("below par");
+      
+    }
+      
+        }
+    }
+
